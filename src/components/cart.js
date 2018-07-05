994b0bb8f826a7fee5500e5597dd94a0d91575f8
linge669 <1421777063@qq.com>
2018-07-05 19:42:19 +0800

    llllllssd

---
+++ src/components/cart.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Cart extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>购物车</h1>
+            </div>
+        );
+    }
+}
+
+export default Cart;
---
+++ src/components/hotel.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Hotel extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>酒店</h1>
+            </div>
+        );
+    }
+}
+
+export default Hotel;
