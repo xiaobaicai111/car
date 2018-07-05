65d114ec3e2dc57c81e77ae5860bca4e445f9c85
yaoxinjie <1072102644@qq.com>
2018-07-05 11:42:00 +0800

    123

---
+++ src/components/Ylbt.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../css/y1.css'
+class Y1 extends Component {
+    render() {
+        return (
+            <div>
+                <h1>哈赛尅</h1>
+            </div>
+        );
+    }
+}
+
+export default Y1;
