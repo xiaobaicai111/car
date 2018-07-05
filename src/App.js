749e293117d40b4e644a3a3d3c7f46a7b2e0890c
xiaobaicai111 <1069857273@qq.com>
2018-07-05 11:34:49 +0800

    pppp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router,Route} from "react-router-dom";
 import Home from "./components/home";
+import "./css/App.css";
 
 class App extends Component {
   render() {
