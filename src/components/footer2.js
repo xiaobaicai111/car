9d2127c110a071e4e374fc3be0e779cd62840a2c
xiaobaicai111 <1069857273@qq.com>
2018-07-06 21:45:11 +0800

    ffff

---
+++ src/components/footer2.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import "../css/footer02.css";
+class Footer02 extends Component {
+    render() {
+        return (
+            <footer className="l_foot">
+            
+            <div className="l_footleft">
+                <p className="l_gy">
+                    <a href="#">关于出国去</a>
+                    <a href="#">加入我们</a>
+                    <a href="#">联系我们</a>
+                    <a href="#">帮助中心</a>
+                    <a href="#">法律声明</a>
+                    <a href="#">友情链接</a>
+                    <a href="#">意见反馈</a>
+                    <a href="#">Business Cooperation</a>
+                    <a href="#">网站地图</a>
+                    <a href="#">分销合作</a>
+                </p>
+                <p >出国去网站制定内容运营商：厦门博游旅游股份有限公司　　唯一指定的境外旅游服务策供方：厦门博游旅游股份有限公司</p>
+               
+                <p>Copyright&copy;2013-2017出国去chuguo.com All rights reserver 厦门博旅旅游股份有限公司 闽ICP备12023338号-1增值电信业务经营许可证 闽B2-20160082</p>
+            </div>
+            <div className="l_footright">
+                <h2>
+                    <span></span>
+                    <b>400-825-3686</b>
+                </h2>
+                <p style={{textAlign:"center"}}>周一至周日：08:88-23:00 节假日：08:00-20:00</p>
+                
+            </div>  
+            </footer>
+        );
+    }
+}
+
+export default Footer02;
--- src/components/register.js
+++ src/components/register.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Head from "./head";
-
+import "../css/register.css";
+import Footer02 from "./footer2"
 class Register extends Component {
     constructor(props){
         super(props)
@@ -10,7 +11,51 @@ class Register extends Component {
         return (
             <div>
                 <Head/>
-                <h1>注册</h1>
+                <div className="l_line"></div>
+                <div className="l_regm">
+                    <h2>30秒快速注册出国去账号</h2>
+                    <div className="l_regbo">
+                        <div className="l_regm_left">
+                            <label>
+                                <p>*账号</p>
+                                 <input type="text" placeholder="手机号/邮箱" ref="phone"/>
+                                 <span className="l_tip">请输入邮箱或手机号</span>
+                            </label>
+                            <label>
+                                <p>*密码</p>
+                                 <input type="password" placeholder="6-20字符" ref="psw"/>
+                                 <span className="l_tip">6-20字符</span>
+                            </label>
+                            <label>
+                                <p>昵称</p>
+                                 <input type="password" placeholder="6-20字符" ref="nickname"/>
+                                 <span className="l_tip">设定一个昵称</span>
+                            </label>
+                            <label>
+                                <p>*验证码</p>
+                                 <input type="text" placeholder="6-20字符" id="l_yzm" ref="yzm"/>
+                                 <em>看不清？<a>换一张</a></em>
+                            </label>
+                            <p className="l_reggroup">
+                                <button className="l_regbtn">立即注册</button>
+                                <i className="l_xy"><input type="checkbox" />我已阅读并同意<a>出国去协议</a></i>
+                            </p>
+                           
+                        </div>
+                        <div className="l_rgmright">
+                            <p>已有出国去账号？<a>点击登录</a></p>
+                            <p>快速登录:</p>
+                            <p>
+                                <a className="l_qq"></a>
+                                <a className="l_sina"></a>    
+                            </p>
+                        </div>
+
+                    </div>
+
+
+                </div>
+                <Footer02/>
             </div>
         );
     }
